b89df3dd0ecf4bdddeb6b54928f78a2398593705
ITSC-conf <itsolctr@ucmail.uc.edu>
2023-12-12 09:47:26 -0500

    made adjustments- calc assignment

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const clickMeButton = document.getElementById('clickme-btn');
-const headerLabel = document.getElementsByTagName('h1')[1];
-// const nameInput = document.getElementById('name-input');
-const nameInput = document.querySelector('#name-input');
+const addButton = document.getElementById('add-btn');
+const input1 = document.querySelector('#input1');
+const input2 = document.querySelector('#input2');
+const list = document.querySelector('#list');
 
-console.log(headerLabel)
+const add = (arg1, arg2) => Number(arg1) + Number(arg2);
 
-clickMeButton.addEventListener('click', () => {
-    const inputValue = nameInput.value;
-    console.log(`You clicked me! ${inputValue}`);
-    headerLabel.textContent = inputValue;
-    headerLabel.style.color = 'green';
+function subtract (arg1, arg2) {
+    return arg1 - arg2;
+}
+
+
+// add button event listenter for hover
+
+addButton.addEventListener('click', () => {
+    const input1Value = input1.value;
+    const input2Value = input2.value;
+
+    const result = add(input1Value, input2Value);
+    const resultNarrative = `${input1Value} + ${input2Value} = ${result}`;
+    console.log(resultNarrative);
 });
+
+
